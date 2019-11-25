67e7fdfd1742a4da3e884e1075bd72b3c2c7988c
go.arai <goarai02151992@gmail.com>
2019-11-25 06:00:50 +0000

    modified validations ver2

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
-  validates :content, length: { maximum: 10 }
+  validates :content, presence: true, length: { maximum: 10 }
 end
