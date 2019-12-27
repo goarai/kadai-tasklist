c9f48ce26ecad5b74dd9a66f5104801760f46df3
go.arai <goarai02151992@gmail.com>
2019-12-27 07:25:46 +0000

    modified rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  
+
   get 'signup', to: 'users#new'
   resources :users, only: [:create]
   
